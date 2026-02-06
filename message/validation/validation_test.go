9190ff780390fda93e0b3b494e6879c8c99cd3c1
Nikita Kryuchkov <nkryuchkov10@gmail.com>
2026-02-06 23:41:02 +0300

    dynamic domain leftovers

--- message/validation/validation_test.go
+++ message/validation/validation_test.go
@@ -103,6 +103,10 @@ func Test_ValidateSSVMessage(t *testing.T) {
 		netCfg:    netCfg,
 		committee: committee,
 	}
+	postBooleLeaderCtx := &leaderTestCtx{
+		netCfg:    postBooleCfg,
+		committee: committee,
+	}
 
 	committeeID := shares.active.CommitteeID()
 	defaultSlot := phase0.Slot(spectestingutils.TestingDutySlot)
@@ -460,7 +464,7 @@ func Test_ValidateSSVMessage(t *testing.T) {
 		_, err = validator.handleSignedSSVMessage(signedSSVMessage, topicID, peerID, receivedAt)
 		expectedErr := ErrWrongDomain
 		expectedErr.got = hex.EncodeToString(wrongDomain[:])
-		domain := netCfg.DomainType
+		domain := netCfg.DomainTypeAtSlot(slot)
 		expectedErr.want = hex.EncodeToString(domain[:])
 		require.ErrorIs(t, err, expectedErr)
 	})
@@ -494,10 +498,10 @@ func Test_ValidateSSVMessage(t *testing.T) {
 		t.Run("aggregator post-fork", func(t *testing.T) {
 			validator := New(postBooleCfg, validatorStore, operators, dutyStore, signatureVerifier).(*messageValidator)
 
-			slot := postBooleCfg.FirstSlotAtEpoch(1)
+			slot := postBooleCfg.FirstSlotAtEpoch(postBooleCfg.SSV.Forks.Boole)
 
-			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainType, shares.active.ValidatorPubKey[:], ssvtypes.RoleAggregator)
-			signedSSVMessage := generateSignedMessage(leaderCtx, ks, badIdentifier, slot)
+			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainTypeAtSlot(slot), shares.active.ValidatorPubKey[:], ssvtypes.RoleAggregator)
+			signedSSVMessage := generateSignedMessage(postBooleLeaderCtx, ks, badIdentifier, slot)
 			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
 
 			receivedAt := postBooleCfg.SlotStartTime(slot)
@@ -508,10 +512,10 @@ func Test_ValidateSSVMessage(t *testing.T) {
 		t.Run("sync committee contribution post-fork", func(t *testing.T) {
 			validator := New(postBooleCfg, validatorStore, operators, dutyStore, signatureVerifier).(*messageValidator)
 
-			slot := postBooleCfg.FirstSlotAtEpoch(1)
+			slot := postBooleCfg.FirstSlotAtEpoch(postBooleCfg.SSV.Forks.Boole)
 
-			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainType, shares.active.ValidatorPubKey[:], ssvtypes.RoleSyncCommitteeContribution)
-			signedSSVMessage := generateSignedMessage(leaderCtx, ks, badIdentifier, slot)
+			badIdentifier := spectypes.NewMsgID(postBooleCfg.DomainTypeAtSlot(slot), shares.active.ValidatorPubKey[:], ssvtypes.RoleSyncCommitteeContribution)
+			signedSSVMessage := generateSignedMessage(postBooleLeaderCtx, ks, badIdentifier, slot)
 			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
 
 			receivedAt := postBooleCfg.SlotStartTime(slot)
@@ -1231,15 +1235,11 @@ func Test_ValidateSSVMessage(t *testing.T) {
 
 		t.Run(message.RunnerRoleToString(spectypes.RoleAggregatorCommittee), func(t *testing.T) {
 			validator := New(postBooleCfg, validatorStore, operators, ds, signatureVerifier).(*messageValidator)
+			slot := postBooleCfg.FirstSlotAtEpoch(postBooleCfg.SSV.Forks.Boole)
 
-			postSlot := postBooleCfg.FirstSlotAtEpoch(epoch)
-			msgID := spectypes.NewMsgID(postBooleCfg.DomainType, encodedCommitteeID, spectypes.RoleAggregatorCommittee)
-			committeeInfo, err := validator.getCommitteeAndValidatorIndices(msgID)
-			require.NoError(t, err)
-
-			leader := qbft.RoundRobinProposer(specqbft.Height(postSlot), specqbft.FirstRound, committeeInfo.committee, postBooleCfg)
-			signedSSVMessage := generateSignedMessageWithLeader(ks, msgID, postSlot, leader)
-			receivedAt := postBooleCfg.SlotStartTime(postSlot + 35)
+			msgID := spectypes.NewMsgID(postBooleCfg.DomainTypeAtSlot(slot), encodedCommitteeID, spectypes.RoleAggregatorCommittee)
+			signedSSVMessage := generateSignedMessage(postBooleLeaderCtx, ks, msgID, slot)
+			receivedAt := postBooleCfg.SlotStartTime(slot + 35)
 			booleTopicID := shares.active.BooleCommitteeSubnet().BooleTopic(postBooleCfg.Beacon.Name)
 
 			_, err = validator.handleSignedSSVMessage(signedSSVMessage, booleTopicID, peerID, receivedAt)
@@ -1663,9 +1663,9 @@ func Test_ValidateSSVMessage(t *testing.T) {
 					committee: committee,
 				}
 
-				slot := defaultSlot + phase0.Slot(i.Load())
-
-				signedSSVMessage := generateSignedMessage(testLeaderCtx, ks, committeeIdentifier, slot)
+				slot := netCfg.FirstSlotAtEpoch(1) + phase0.Slot(i.Load())
+				msgID := spectypes.NewMsgID(tc.cfg.DomainTypeAtSlot(slot), encodedCommitteeID, committeeRole)
+				signedSSVMessage := generateSignedMessage(testLeaderCtx, ks, msgID, slot)
 
 				receivedAt := netCfg.SlotStartTime(slot)
 
